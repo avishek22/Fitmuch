e40bfb3c35463dab3225becebbdcbd01a30bd372
Avishek <avishekbhattacharjee35@gmail.com>
2021-06-18 22:00:42 +0530

    frontend integration

--- frontend/src/screens/Day/index.js
+++ frontend/src/screens/Day/index.js
@@ -1,10 +1,27 @@
 import { useNavigation } from '@react-navigation/native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View, Text, TouchableOpacity } from 'react-native'
 import { EXERCISE, EXERCISE_DETAIL } from '../../constants/routeNames'
 
 const Day = () => {
     const {navigate}=useNavigation()
+    useEffect(() => {
+        fetch("http://10.0.2.2:4000/allday", {
+            headers:{
+                'Content-Type': 'application/json'
+            }
+          
+        })
+          .then((res) => res.json())
+          .then((result) => {
+            console.log(result);
+            console.log('ok')
+            
+          }).catch((err)=>{
+              console.log(err)
+              console.log('errrrror')
+          });
+      }, []);
     return (
         <View>
             <Text>Hello from Day</Text>
